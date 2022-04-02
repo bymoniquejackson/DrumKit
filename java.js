5ed77b67bc3bee182035d1237b732df004b9d413
Monique Jackson <bymoniquejackson@gmail.com>
2022-04-02 13:43:18 +0100

    finally got the keys AND click working

--- java.js
+++ java.js
@@ -1,80 +1,85 @@
-let numberOfButtons = document.querySelectorAll(".button").length;
+let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (let i = 0; i < numberOfButtons; i++) {
 
-document.querySelectorAll(".button")[i]
-    .addEventListener("click", function() {
-    const buttonStyle = this.innerHTML;
-    sound(buttonStyle);
-    animation(buttonStyle);
+for (let i = 0; i < numberOfDrumButtons; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    let buttonInnerHTML = this.innerHTML;
+    Sound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+
 });
+
 }
 
-
 document.addEventListener("keypress", function(event) {
-    sound(event.key);
-    animation(event.key);
+Sound(event.key);
+buttonAnimation(event.key);
 });
 
-function sound(key) {
-    switch (key) {
-    case "1":
+function Sound(key) {
+
+switch (key) {
+    case "a":
         let boom = new Audio("sounds/boom.wav");
         boom.play();
         break;
 
-    case "2":
-    let clap = new Audio("sounds/clap.wav");
-    clap.play();
-    break;
-    
-    case "3":
-        let hiHat = new Audio("sounds/hihat.wav");
-        hiHat.play();
+    case "s":
+        let clap = new Audio("sounds/clap.wav");
+        clap.play();
         break;
 
-    case "4":
-        let kick = new Audio("sounds/kick.wav");
+    case "d":
+        let hihat = new Audio('sounds/hihat.wav');
+        hihat.play();
+        break;
+
+    case "f":
+        let kick = new Audio('sounds/kick.wav');
         kick.play();
         break;
 
-    case "5":
-        let openHat = new Audio("sounds/openhat.wav");
-        openHat.play();
+    case "g":
+        let openhat = new Audio('sounds/openhat.wav');
+        openhat.play();
         break;
 
-    case "6":
-        let ride = new Audio("sounds/ride.wav");
+    case "h":
+        let ride = new Audio('sounds/ride.wav');
         ride.play();
         break;
 
-    case "7":
-        let snare = new Audio("sounds/snare.wav")
+    case "j":
+        let snare = new Audio('sounds/snare.wav');
         snare.play();
         break;
 
-    case "8":
-        let tink = new Audio("sounds/tink.wav")
+    case "k":
+        let tink = new Audio('sounds/tink.wav');
         tink.play();
-        break;
+            break;
 
-    case "9":
-        let tom = new Audio("sounds/tom.wav")
+    case "l":
+        let tom = new Audio('sounds/tom.wav');
         tom.play();
-        break;
+            break;
+
 
         default: console.log(key);
+
 }
 }
 
 
+function buttonAnimation(currentKey) {
 
-function animation(currentKey) {
-    let activeButton = document.querySelector("." + currentKey);
-    
-    activeButton.classList.add("animation");
-    
-    setTimeout(function() {
-    activeButton.classList.remove("animation");
-    }, 100);
+let activeButton = document.querySelector("." + currentKey);
+
+activeButton.classList.add("pressed");
+
+setTimeout(function() {
+    activeButton.classList.remove("pressed");
+}, 100);
+
 }
